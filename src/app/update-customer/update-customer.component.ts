3d6a201ccd3f3d513022a3135c8f222d31916382
Slawek <Koboltq@gmail.com>
2018-08-06 23:37:54 +0200

    test

--- src/app/update-customer/update-customer.component.ts
+++ src/app/update-customer/update-customer.component.ts
@@ -38,12 +38,14 @@ export class UpdateCustomerComponent implements OnInit {
   }
 
   getWeather(city) {
-    const url = "https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+city.toString()+"') and u='c'&format=json";
+    // const url = "https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+city.toString()+"') and u='c'&format=json";
+    const url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+city.toString()+'%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     this.http.get(url).subscribe(
       (res:Response) => {
         console.log("city " + city);
         this.weather = res.json();
         console.log(this.weather);
+        console.log(this.weather['temp']);
       }
     )
   }
